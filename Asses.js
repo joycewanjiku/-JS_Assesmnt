c1b4ff4ac34ffafec691a75ebec2d657467f4da4
student <joycemumbiiwanjiku@gmail.com>
2023-04-11 08:59:57 +0300

    js

---
+++ Asses.js
@@ -0,0 +1,51 @@
+// 1. Write a function that takes in a string and returns it when reversed
+//  let food = “eating”
+function food(strings){
+    if(i=0 ){
+
+    }
+return
+}
+
+
+
+// 2. Write a function that takes in the following array and consoles the target if it is found else
+// null
+// let num = [2,8,0,23,5,45,76]
+// Target = 23
+function divide(nums){
+    (i=0 <=length.i++);
+    right=Math.floor(0,middle);
+    left=Math.floor(middle);
+    EmptyArray=[]
+
+    let left=left.push();
+
+
+
+}
+// 3. Given years between 2000 and 2023, console all the leap years in the following
+// sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
+const year=("2020 is a leap year");
+console.log(year.concat);
+// 4. Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
+// “Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
+
+// 5. Write a function that takes in an array of numbers and returns an array that has all
+// elements multiplied by 4.
+
+let numArray = [12,87,45,75,23,64,73]
+function multiply(numArray){
+if(i=1<= length.i++){
+
+}
+}
+    let num=(nun[i])
+
+
+// 6. Write a function that takes in an array of strings and returns an array with every element
+// turned into a number
+// let nums = [“10”,”24”,”45”,”56”,”67”]
+let numbers=['10','24','45','56','67']
+let nums=numbers.joinArray
+console.log(mumbers.joinArray)
